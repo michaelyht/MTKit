8befccb36d565b8899d0453319516888d7b118ed
Michael <htlingday@hotmail.com>
2018-02-22 11:44:49 +0800

    引导头文件

--- MTKit/MTKit/MTKit.h
+++ MTKit/MTKit/MTKit.h
@@ -17,3 +17,11 @@ FOUNDATION_EXPORT const unsigned char MTKitVersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <MTKit/PublicHeader.h>
 
 
+#import <UIKit/UIKit.h>
+#import "TT_CategoryHeader.h"
+#import "BaseService.h"
+#import "TTMediator.h"
+#import "TT_Define_App.h"
+#import "TT_Define_Singleton.h"
+#import "TT_Utils_Header.h"
+#import "TT_ComponetHeader.h"
